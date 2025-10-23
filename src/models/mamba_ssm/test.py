b2fc069eace590d7de8eb48e062bb5b2b14eb8ef
anhndv1 <vietanhnd.2002@gmail.com>
2025-10-23 22:35:47 +0700

    Test mamba model

--- src/models/mamba_ssm/test.py
+++ src/models/mamba_ssm/test.py
@@ -1,16 +1,15 @@
-
 from .modules.mamba2 import Mamba2
 import torch
-
-batch, length, dim = 2, 64, 32
+batch, length, dim = 2, 64, 16
 x = torch.randn(batch, length, dim).to("cuda")
-
 model = Mamba2(
     # This module uses roughly 3 * expand * d_model^2 parameters
-    d_model=dim, # Model dimension d_model
+    d_model=16, # Model dimension d_model
     d_state=64,  # SSM state expansion factor, typically 64 or 128
     d_conv=4,    # Local convolution width
-    expand=2,    # Block expansion factor
+    expand=2,
+    headdim=32,    # Block expansion factor
 ).to("cuda")
 y = model(x)
 assert y.shape == x.shape
+print("Chạy thành công!")
